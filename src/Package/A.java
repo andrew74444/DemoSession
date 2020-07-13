f1367bb0e8877474791b64565186eec6e874d6e2
QA Team <andrew74444@gmail.com>
2020-07-13 08:43:36 +0530

    first commit

---
+++ src/Package/A.java
@@ -0,0 +1,19 @@
+package Package;
+
+public class A {
+
+	public static void main(String[] args) {
+		
+		A a = new A();
+		int z = a.sum(10, 20);
+		System.out.println(z);		
+	}
+
+	public int sum (int x, int y){
+		int z = x+y;
+		return z;
+		} 
+
+	
+	
+}
---
+++ src/Package/MyFirstClass.java
@@ -0,0 +1,102 @@
+package Package;
+
+public class MyFirstClass {
+
+	public static void main(String[] args) {
+		
+		
+	    System.out.println("Hello World");
+	    System.out.println("Hello World 1");
+	    System.out.println("Hello World 2");
+	    System.out.println("Hello World 3");
+	    
+	    MyFirstClass myFirstClass = new MyFirstClass();
+	    
+	    System.out.println(myFirstClass);
+	    System.out.println(myFirstClass);
+	    System.out.println(myFirstClass);
+	    System.out.println(myFirstClass);
+	    System.out.println(myFirstClass);
+	    
+	    System.out.println("");
+	    System.out.println("");
+	    System.out.println("");
+	    System.out.println(new MyFirstClass());
+	    System.out.println(new MyFirstClass());
+	    System.out.println(new MyFirstClass());
+	    
+	    int i = 2;
+	    
+	    System.out.println("i is "+i);
+	    
+	    long l = 76476576l;
+	    System.out.println("l is "+l);
+	    
+	    float f = 2342.34f;
+	    System.out.println("f is  "+f);
+	    double d= 2987.29429235345436536274;
+	    System.out.println("d is "+d);
+	    char c = 'K'; 
+	    System.out.println("C is "+c);
+	    
+	    boolean t = true;
+	    System.out.println(t);
+	    
+	    boolean x = false;
+	    System.out.println(x);
+	    
+	    
+	    String s = "We " + "are " + "learning " + "java" + ".";
+	    
+	    System.out.println(s);
+	    
+	    
+	    int p = 20;
+	    int q = 30;
+	    int r = p+q;
+	    
+	    System.out.println("value of r is " + r);
+	    
+	    
+	    
+	    int Y = 200;
+	    
+	    
+	    
+	    
+	    System.out.println("value of Y after Increment is " + ++Y);
+	    System.out.println("value of Y is " + Y);
+	    System.out.println("value of Y after decrement is " + --Y);
+	    
+	    
+	    if (Y!=100) {
+	    	System.out.println("im from If condition");  	
+	    	
+	    }
+	    
+	    int m = 40;
+	    int n = 30;
+	    
+	    
+	    if(m<=n) {
+	    	System.out.println("m is less than n or equal");
+	    }
+	    
+	    
+	    
+	    
+	    
+	   if (m < 10 && n > 1) {
+		   System.out.println("M is less than 10 and N is Greater than 1" );
+	   }
+	    
+	   if (m == 5 || n == 5) {
+		   System.out.println("M or N is equal to 5" );
+	   }
+	    
+	    
+	    
+	    
+	}
+
+}
